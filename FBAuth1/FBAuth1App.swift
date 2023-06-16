654f841d726ec8e6b087c2d13502504a18b1c542
Chris <f.chrisgoodson@icloud.com>
2023-06-15 19:34:56 -0500

    Initial Commit

---
+++ FBAuth1/FBAuth1App.swift
@@ -0,0 +1,17 @@
+//
+//  FBAuth1App.swift
+//  FBAuth1
+//
+//  Created by Chris on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FBAuth1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
